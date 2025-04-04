0f8a4d6278a0bd49a76971b4ad1df035628012c5
Ruslan <ruslankhakimullin@gmail.com>
2025-04-04 12:28:43 +0300

    Tests pass, norminette ok. Double check the Makefile, esp the MLX part

--- valrun.sh
+++ valrun.sh
@@ -1,1 +1,1 @@
-valgrind --suppressions=suppression.supp --track-fds=yes --leak-check=full --show-leak-kinds=all ./cub3D maps/map.cub
+valgrind --suppressions=suppression.supp --track-fds=yes --leak-check=full --show-leak-kinds=all ./cub3D maps/map3.cub
